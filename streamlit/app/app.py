5de412446f7d9c944b4a74facefb5ea8bd4fff77
castrojfelipe <castro.jfelipe@gmail.com>
2025-12-07 14:51:03 -0300

    form checkbox data

--- streamlit/app/app.py
+++ streamlit/app/app.py
@@ -13,17 +13,31 @@ conn = trino.dbapi.connect(
 with st.form("my_form"):
     st.write("Selecciona los campos:")
     checkbox = {
-        'empresa.id': st.checkbox("Empresa ID"),
-        'empresa.name': st.checkbox("Empresa nombre"),
-        'empresa.test': st.checkbox("Empresa de prueba"),
-        'trabajador.id': st.checkbox("Empleado ID"),
-        'trabajador.name': st.checkbox("Empleado nombre"),
-        'contrato.id': st.checkbox("Contrato ID"),
-        'contrato.contract_id': st.checkbox("Contrato código"),
-        'contrato.since': st.checkbox("Contrato Desde"),
-        'contrato.until': st.checkbox("Contrato Hasta"),
-        'contrato.position': st.checkbox("Contrato Cargo"),
+        'empresa.id': st.checkbox("empresa.id"),
+        'empresa.name': st.checkbox("empresa.name"),
+        'empresa.test': st.checkbox("empresa.test"),
+        'trabajador.id': st.checkbox("trabajador.id"),
+        'trabajador.name': st.checkbox("trabajador.name"),
+        'contrato.id': st.checkbox("contrato.id"),
+        'contrato.contract_id': st.checkbox("contrato.contract_id"),
+        'contrato.since': st.checkbox("contrato.since"),
+        'contrato.until': st.checkbox("contrato.until"),
+        'contrato.position': st.checkbox("contrato.position"),
     }
+    option = st.selectbox(
+        "Filtro",
+        ("empresa.id", "empresa.name", "empresa.test", "trabajador.id", 
+         "trabajador.name", "trabajador.id", "contrato.id", "contrato.since",
+         "contrato.until", "contrato.position")
+    )
+    operator = st.selectbox("operador", ("=", "<", ">", "<=", ">=", "<>"))
+    if option in ["empresa.id", "trabajador.id", "contrato.id"]:
+        input = st.number_input("")
+    elif option in ["empresa.name", "trabajador.name", "contrato.position"]:
+        input = st.text_input("")
+    elif option in ["contrato.since", "contrato.until"]:
+        input = st.date_input("")
+
     submitted = st.form_submit_button("Generar")
     if submitted:
         select = []
@@ -31,18 +45,19 @@ with st.form("my_form"):
         for key in checkbox.keys():
             if checkbox[key]:
                 select.append(key)
-                columns.append(key)
-        select = (',').join(select)
-
+        select_comma = (',').join(select)
+        where = f"and {option} {operator} {input}"
         cur = conn.cursor()
         cur.execute(
-            f"select {select} "
+            f"select {select_comma} "
             "from iceberg.nyc.company empresa, "
             "unnest(empresa.employee) trabajador,"
             "unnest(trabajador.contract) contrato "
+            "where 1=1 "
+            f"{where}"
         )
         rows = cur.fetchall()
-        df = pd.DataFrame(rows, columns=columns).drop_duplicates()
+        df = pd.DataFrame(rows, columns=select).drop_duplicates()
 
         st.dataframe(df)
 
