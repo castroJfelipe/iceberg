229f93ae6aa1ebb205b698583c65e66db2f11338
castrojfelipe <castro.jfelipe@gmail.com>
2025-12-07 14:18:30 -0300

    form checkbox data

--- streamlit/app/app.py
+++ streamlit/app/app.py
@@ -10,18 +10,40 @@ conn = trino.dbapi.connect(
     catalog='iceberg'
 )
 
-cur = conn.cursor()
-cur.execute(
-    "select cmp.id, cmp.name, cmp.test, "
-    "   emp.id employee_id, emp.name employee_name, "
-    "   ctc.id contract_id, ctc.contract_id contract_contract_id, "
-    "   ctc.since contract_since, ctc.until contract_until, "
-    "   ctc.position contract_position "
-    "from iceberg.nyc.company cmp, "
-    "unnest(cmp.employee) emp,"
-    "unnest(emp.contract) ctc "
-)
-rows = cur.fetchall()
-df = pd.DataFrame(rows)
-print(df.head(2))
-st.dataframe(df)
+with st.form("my_form"):
+    st.write("Selecciona los campos:")
+    checkbox = {
+        'empresa.id': st.checkbox("Empresa ID"),
+        'empresa.name': st.checkbox("Empresa nombre"),
+        'empresa.test': st.checkbox("Empresa de prueba"),
+        'trabajador.id': st.checkbox("Empleado ID"),
+        'trabajador.name': st.checkbox("Empleado nombre"),
+        'contrato.id': st.checkbox("Contrato ID"),
+        'contrato.contract_id': st.checkbox("Contrato código"),
+        'contrato.since': st.checkbox("Contrato Desde"),
+        'contrato.until': st.checkbox("Contrato Hasta"),
+        'contrato.position': st.checkbox("Contrato Cargo"),
+    }
+    submitted = st.form_submit_button("Generar")
+    if submitted:
+        select = []
+        columns = []
+        for key in checkbox.keys():
+            if checkbox[key]:
+                select.append(key)
+                columns.append(key)
+        select = (',').join(select)
+
+        cur = conn.cursor()
+        cur.execute(
+            f"select {select} "
+            "from iceberg.nyc.company empresa, "
+            "unnest(empresa.employee) trabajador,"
+            "unnest(trabajador.contract) contrato "
+        )
+        rows = cur.fetchall()
+        df = pd.DataFrame(rows, columns=columns).drop_duplicates()
+
+        st.dataframe(df)
+
+
